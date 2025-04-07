a7616ad2b97c6c44a8ecb9d05525a12ef42d1158
Anderson Nguyen <145573232+virgilreturns@users.noreply.github.com>
2025-04-07 15:31:44 -0400

    Update aml-stm32f401.h

--- aml-core/aml-stm32f401.h
+++ aml-core/aml-stm32f401.h
@@ -1,15 +1,39 @@
 /*
-  @filename        aml-stm32f401.h
+  @filename        "aml-stm32f401.h"
   @author          Anderson Nguyen
   @date            4/7/2025
 */
 
+#ifndef AML_STM32F401_H
+#define AML_STM32F401_H
+
 #include "stm32f401xe.h"
+
+typedef enum {
+  GPIO_PIN_RESET   = 0,
+  GPIO_PIN_SET     = 1,
+} GPIO_PinState;
+
+typedef struct {
+  uint16_t pin,
+  uint16_t 
+} GPIO_InitStruct;
+
+static inline void AML_GPIO_Init(GPIO_TypeDef){
+  
+};
+static inline GPIO_PinState AML_GPIO_Read(GPIO_TypeDef* port, uint8_t pin);{
+  return port->
+}
+static inline void AML_GPIO_Write(GPIO_TypeDef* port, uint8_t pin);
+
 
 /* Sample alias definitions
 #define SPI_MOSI_Pin             GPIO_PIN_13
 #define SPI_MOSI_GPIO_Port       GPIOA
 */
+
+#endif // AML_STM32F401_H
 
 
 
