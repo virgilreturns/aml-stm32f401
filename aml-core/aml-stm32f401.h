0c9090b8d65aa9f77c0d66baeaa89d67fc5038e0
Anderson Nguyen <145573232+virgilreturns@users.noreply.github.com>
2025-04-07 14:32:33 -0400

    Create aml-stm32f401.h

---
+++ aml-core/aml-stm32f401.h
@@ -0,0 +1,15 @@
+/*
+  @filename        aml-stm32f401.h
+  @author          Anderson Nguyen
+  @date            4/7/2025
+*/
+
+#include "stm32f401xe.h"
+
+/* Sample alias definitions
+#define SPI_MOSI_Pin             GPIO_PIN_13
+#define SPI_MOSI_GPIO_Port       GPIOA
+*/
+
+
+
